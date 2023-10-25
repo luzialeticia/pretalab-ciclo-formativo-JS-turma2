9b55a763d8a3b119f1baa492d030be0491346d7a
Leticia Luzia <leticialuzia89@gmail.com>
2023-10-24 23:08:33 -0300

    Exemplos em aula

---
+++ semana01/aula01/ola-mundo/variaveis.js
@@ -0,0 +1,9 @@
+// Variável que não muda valor CONSTANTE
+const nome = "Letícia"
+const sobrenome = "Furtado"
+let idade = 32
+
+console.log(nome)
+console.log(sobrenome)
+console.log(idade)
+console.log(nome, sobrenome, idade)
