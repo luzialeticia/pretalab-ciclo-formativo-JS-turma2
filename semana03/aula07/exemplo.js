176103c6b900a415ef986d20e37daf3c27438b8b
Leticia Luzia <leticialuzia@icloud.com>
2023-11-07 20:54:35 -0300

    Exemplo em aula

---
+++ semana03/aula07/exemplo.js
@@ -0,0 +1,16 @@
+for(let i = 1; i <= 9; i++){
+    console.log(`9x${i} = ${9*i}`)
+}
+
+let numero = 1;
+while(numero<=9) {
+    console.log(`9x${numero} = ${9*numero}`)
+    numero++
+}
+
+let numero2 = 1;
+
+do{
+   console.log(`9x${numero2} = ${9*numero2}`)
+   numero2++
+}while(numero2 <= 9)
