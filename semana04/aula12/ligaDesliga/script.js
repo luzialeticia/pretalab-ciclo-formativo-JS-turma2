23b0c3753d83d087c646fa4ec1f165a310360613
Leticia Luzia <leticialuzia@icloud.com>
2023-11-18 11:40:42 -0300

    Solução completa do changeBg()

--- semana04/aula12/ligaDesliga/script.js
+++ semana04/aula12/ligaDesliga/script.js
@@ -1,5 +1,7 @@
 const imagem = document.querySelector('img')
 const background = document.body
+const h1 = document.querySelector('h1')
+const gif = document.querySelector('.imagem')
 
 //background-color
 background.style.backgroundColor = "red"
@@ -7,4 +9,16 @@ background.style.backgroundColor = "red"
 
 const changeBg = () => {
     background.style.backgroundColor = 'black'
+
+    if (background.style.backgroundColor == "red") {
+        background.style.backgroundColor = "black"
+        h1.innerText = "Desligado"
+        h1.style.color = "white"
+        gif.src = "https://media.giphy.com/media/N4a7e1ftdbQWc/giphy.gif"
+      } else {
+        background.style.backgroundColor = "red"
+        h1.innerText = "Ligado"
+        h1.style.color = "black"
+        gif.src = "https://media.giphy.com/media/3KVRMtLXfORVosk6TW/giphy.gif"
+      }
 }
